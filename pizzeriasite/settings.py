6da168a2433a478431b6f49c673604c94b470762
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2023-11-22 00:04:57 +0000

    Fixed error disallowed host

--- pizzeriasite/settings.py
+++ pizzeriasite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['felipezanini.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['zanini-pizzeria-0279eae282e5.herokuapp.com', 'localhost']
 
 
 # Application definition
