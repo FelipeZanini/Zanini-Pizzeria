8c4315441f0c725006168440a3a4729d8b1749a5
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2023-11-22 00:53:45 +0000

    Created register user in views.py

--- zanini/urls.py
+++ zanini/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='menu'),
     path('menu_items/', views.menu_items, name='menu_items'),
+    path('register/', views.register_view, name='register'),
 ]
--- zanini/views.py
+++ zanini/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from .forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
 
 def home(request):
     return render(request, 'index.html')
@@ -12,3 +13,17 @@ def menu(request):
 
 def menu_items(request):
     return render(request, 'menu_items.html')
+
+def register_view(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            messages.success(request, 'Signup success!')
+            return redirect('home')
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
