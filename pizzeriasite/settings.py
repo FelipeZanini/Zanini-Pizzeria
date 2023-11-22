2e8ac794219714ae7995c1d689b2abcaf3abea75
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2023-11-22 00:37:00 +0000

    Added the urls.py

--- pizzeriasite/settings.py
+++ pizzeriasite/settings.py
@@ -28,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['zanini-pizzeria-0279eae282e5.herokuapp.com', 'localhost']
 
