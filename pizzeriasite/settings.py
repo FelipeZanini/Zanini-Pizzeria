ab5273766c370e31e5c472feb37dc8d4c76409da
Felipe Silva <felipe_zanini@hotmail.com>
2024-03-03 14:42:15 +0000

    Change style when buttons are hover over from orange to grey

--- pizzeriasite/settings.py
+++ pizzeriasite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zanini-pizzeria-0279eae282e5.herokuapp.com']
+ALLOWED_HOSTS = ['zanini-pizzeria-0279eae282e5.herokuapp.com', '8000-felipezanin-zaninipizze-icc9zln539j.ws-eu108.gitpod.io']
 
 
 # Application definition
