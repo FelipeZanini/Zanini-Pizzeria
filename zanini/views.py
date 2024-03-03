a26e1f5416e8b5a9f8549485a33cbaa5c0a34f13
Felipe Silva <felipe_zanini@hotmail.com>
2024-03-03 13:48:32 +0000

    Add login required to logout, edit booking and delete account

--- zanini/views.py
+++ zanini/views.py
@@ -1,4 +1,5 @@
 from datetime import date
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
@@ -23,27 +24,39 @@ def book_table(request):
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
-            ts = form.cleaned_data.get("table_size")
-            bt = form.cleaned_data.get("booking_time")
+            table_size = form.cleaned_data.get("table_size")
+            booking_time = form.cleaned_data.get("booking_time")
             dt = form.cleaned_data.get("date")
-            if Reservation.check_table_avaliability(ts, bt, dt):
-                reservation = Reservation(table_size=bt,
-                                          booking_time=bt, date=dt)
+            if Reservation.check_table_avaliability(table_size, booking_time,
+                                                    dt):
+                reservation = Reservation(table_size=table_size,
+                                          booking_time=booking_time,
+                                          date=dt)
                 if not Reservation.objects.filter(user=request.user).exists():
-                    reservation.user = request.user
-                    reservation.save()
-                    messages.success(request, 'Table booked successfully!')
-                    return redirect('home')
-                messages.success(request, 'One reservation at a time allowed!')
+                    if reservation.date >= date.today():
+                        reservation.user = request.user
+                        reservation.save()
+                        messages.success(request, 'Table booked successfully!')
+                        return redirect('home')
+                    else:
+                        messages.success(request,"You can't time travel, yet.")
+                        return redirect('book_table')
+                else:
+                    messages.success(request,
+                                     'One reservation at a time allowed!')
+                    return redirect('book_table')
+            else:
+                messages.success(request,
+                                 'Sorry, Tables full for this date and time.')
                 return redirect('book_table')
-            messages.success(request,
-                             'Sorry, Tables full for this date and time.')
-            return redirect('book_table')
+    Reservation.delete_old_dates()
     return render(request, 'book_table.html', {'form': form})
 
 
+@login_required
 def delete_reservation(request, id_item):
-    reservation = Reservation.objects.get(id=id_item)
+    reservation = Reservation.objects.get(id=id_item, user=request.user)
+    print(reservation)
     if not reservation.date == date.today():
         reservation.delete()
         messages.success(request, 'Reservation canceled!')
@@ -52,12 +65,14 @@ def delete_reservation(request, id_item):
     return redirect('user_page')
 
 
+@login_required
 def update_reservation(request, id_item):
     form = ReservationForm()
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
-            reservation = Reservation.objects.get(id=id_item)
+            reservation = Reservation.objects.get(id=id_item,
+                                                  user=request.user)
             reservation.table_size = form.cleaned_data.get("table_size")
             reservation.booking_time = form.cleaned_data.get("booking_time")
             reservation.date = form.cleaned_data.get("date")
@@ -67,11 +82,13 @@ def update_reservation(request, id_item):
                 reservation.save()
                 messages.success(request, 'Reservation updated!')
                 return redirect('home')
-            messages.success(request,
-                             'Sorry, Tables full for this date and time.')
+            else:
+                messages.success(request,
+                                'Sorry, Tables full for this date and time.')
     return render(request, 'book_table.html', {'form': form})
 
 
+@login_required
 def user_page(request):
     reservations = Reservation.objects.filter(user=request.user)
     return render(request, 'user_page.html', {'reservations': reservations})
@@ -95,6 +112,7 @@ def login_view(request):
     return render(request, "login.html", {'form': form})
 
 
+@login_required
 def logout_view(request):
     logout(request)
     messages.success(request, "You're logged out!")
@@ -116,6 +134,7 @@ def register_view(request):
     return render(request, 'register.html', {'form': form})
 
 
+@login_required
 def delete_account(request):
     user = request.user
     user.delete()
